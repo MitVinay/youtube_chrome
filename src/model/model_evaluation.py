9595df77d95fca3ab80a1e502f5f89c84a350848
MitVinay <vm05011996@gmail.com>
2024-12-31 05:40:32 +1100

    Done

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -15,7 +15,7 @@ from mlflow.models import infer_signature
 import dagshub
 import dotenv
 
-# Set up DagsHub credentials for MLflow trackings
+# Set up DagsHub credentials for MLflow trackingss
 dagshub_token = os.getenv("DAGSHUB_PAT")
 print(dagshub_token)
 if not dagshub_token:
